a10c82a70b352bca6132f58ae8bd44a1f0060003
Anthony FERREYROLLES <anthony.ferreyrolles@edu.ece.fr>
2021-11-30 13:22:46 +0100

    Project creation

---
+++ big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/BigDataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ece_ing5app_2021.big_data_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataProjectApplication.class, args);
+	}
+
+}
---
+++ big_data_project/src/test/java/com/ece_ing5app_2021/big_data_project/BigDataProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece_ing5app_2021.big_data_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
