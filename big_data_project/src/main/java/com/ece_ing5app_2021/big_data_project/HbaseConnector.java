b267c4609b1c610b0eef139c185e0c0ea1e64bb5
Anthony FERREYROLLES <anthony.ferreyrolles@edu.ece.fr>
2021-12-08 19:50:56 +0100

    using /etc/krb5.keytab

--- big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/HbaseConnector.java
+++ big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/HbaseConnector.java
@@ -25,7 +25,6 @@ public class HbaseConnector {
         conf.addResource(new Path(coresite));
 
         UserGroupInformation.setConfiguration(conf);
-        UserGroupInformation.
         UserGroupInformation.loginUserFromKeytab(principal, keyTabPath);
         Connection connection = ConnectionFactory.createConnection(conf);
         HBaseAdmin.available(conf);
