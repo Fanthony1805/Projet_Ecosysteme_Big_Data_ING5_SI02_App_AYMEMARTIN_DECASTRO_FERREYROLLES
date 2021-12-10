8f0b765fdabaa4546fe42e2e9165eebca03dd49e
Anthony FERREYROLLES <anthony.ferreyrolles@edu.ece.fr>
2021-12-10 18:49:07 +0100

    Testing HBase config

--- big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/GreetingController.java
+++ big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/GreetingController.java
@@ -20,6 +20,11 @@ public class GreetingController {
 	private static Connection conn;
 	// Instantiating HTable class
 	private static Table table;
+	private static int userID;
+	private static int channelID;
+	private static int MessageID;
+	private static String username;
+	private static String channelname;
 
 	@GetMapping("/try_connection")
 	public String greeting(@RequestParam(value = "name", defaultValue = "Anthony") String name) {
@@ -41,7 +46,7 @@ public class GreetingController {
 		return "Connection Failed\n";
 	}
 
-	@GetMapping("/get_values")
+	@GetMapping("/get_user")
 	public String getValues() {
 		try {
 			conn = HbaseConnector.getConnectionByFile("/home/a.ferreyrolles-ece/mykey.keytab",
@@ -49,19 +54,18 @@ public class GreetingController {
 					"a.ferreyrolles-ece@AU.ADALTAS.CLOUD");
 
 			table = conn.getTable(TableName.valueOf("ece_2021_fall_app_2:AFerreyrolles"));
+			
+			userID = 1;
 			// Instantiating Get class
-			Get g = new Get(Bytes.toBytes("user"));
+			Get g = new Get(Bytes.toBytes("u" + userID));
 
 			// Reading the data
 			Result result = table.get(g);
 
-			byte[] value = result.getValue(Bytes.toBytes("username"), Bytes.toBytes(""));
-			String valueStr = Bytes.toString(value);
-
-			System.out.println("value : " + value);
-
-			System.out.println("valueStr : " + valueStr);
-			return valueStr + "\n";
+			byte[] value = result.getValue(Bytes.toBytes("user"), Bytes.toBytes("username"));
+			username = Bytes.toString(value);
+			
+			return username + "\n";
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -69,7 +73,7 @@ public class GreetingController {
 		return "Couldn't get values\n";
 	}
 	
-	@GetMapping("/put_values")
+	@GetMapping("/put_user")
 	public String putValues() {
 		try {
 			conn = HbaseConnector.getConnectionByFile("/home/a.ferreyrolles-ece/mykey.keytab",
@@ -77,9 +81,49 @@ public class GreetingController {
 					"a.ferreyrolles-ece@AU.ADALTAS.CLOUD");
 
 			table = conn.getTable(TableName.valueOf("ece_2021_fall_app_2:AFerreyrolles"));
+			
+			userID = 1;
+			username = "user1";
 			// Instantiating Get class
-			Put put = new Put(Bytes.toBytes("user"));
-			put.addColumn(Bytes.toBytes("username"), Bytes.toBytes(""), Bytes.toBytes("awesomeValue"));
+			Put put = new Put(Bytes.toBytes("u"+userID));
+			put.addColumn(Bytes.toBytes("user"), Bytes.toBytes("username"), Bytes.toBytes(username));
+			put.addColumn(Bytes.toBytes("user"), Bytes.toBytes("email"), Bytes.toBytes("user1@test.com"));
+			put.addColumn(Bytes.toBytes("user"), Bytes.toBytes("password"), Bytes.toBytes("Password"));
+			//Add as many columns as you want
+
+			table.put(put);
+			
+			return "Value successfully added\n";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Couldn't add the value\n";
+	}
+	
+	@GetMapping("/put_channel")
+	public String putMessage() {
+		try {
+			conn = HbaseConnector.getConnectionByFile("/home/a.ferreyrolles-ece/mykey.keytab",
+					"/etc/hadoop/conf/core-site.xml", "/etc/krb5.conf", "/etc/hbase/conf/hbase-site.xml",
+					"a.ferreyrolles-ece@AU.ADALTAS.CLOUD");
+
+			table = conn.getTable(TableName.valueOf("ece_2021_fall_app_2:AFerreyrolles"));
+			
+			channelID = 1;
+			channelname = "Channel1";
+			// Instantiating Get class
+			Put put = new Put(Bytes.toBytes("c" + channelID));
+			put.addColumn(Bytes.toBytes("channel"), Bytes.toBytes("owner"), Bytes.toBytes("u" + userID));
+			put.addColumn(Bytes.toBytes("channel"), Bytes.toBytes("channelname"), Bytes.toBytes(channelname));
+			
+			//Add as many columns as you want
+
+			table.put(put);
+			
+			put = new Put(Bytes.toBytes("u" + userID +"_c" + channelID));
+			put.addColumn(Bytes.toBytes("user"), Bytes.toBytes("username"), Bytes.toBytes(username));
+			put.addColumn(Bytes.toBytes("channel"), Bytes.toBytes("channelname"), Bytes.toBytes(channelname));
 			//Add as many columns as you want
 
 			table.put(put);
