28018dad008ad2d5b145aea7b98dc1f63478cc8b
Anthony FERREYROLLES <anthony.ferreyrolles@edu.ece.fr>
2021-12-10 17:08:22 +0100

    Reorganise code

--- big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/GreetingController.java
+++ big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/GreetingController.java
@@ -1,13 +1,11 @@
 package com.ece_ing5app_2021.big_data_project;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -17,45 +15,55 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	private static Connection conn;
 
-	@SuppressWarnings("finally")
-	@GetMapping("/greeting")
+	@GetMapping("/try_connection")
 	public String greeting(@RequestParam(value = "name", defaultValue = "Anthony") String name) {
-		Connection conn;
-		
-		 try {
-			 conn = HbaseConnector.getConnectionByFile("/home/a.ferreyrolles-ece/mykey.keytab","/etc/hadoop/conf/core-site.xml", "/etc/krb5.conf", "/etc/hbase/conf/hbase-site.xml", "a.ferreyrolles-ece@AU.ADALTAS.CLOUD");
-			 
-			 Admin admin = conn.getAdmin();
-		     System.out.println(admin.tableExists(TableName.valueOf("ece_2021_fall_app_2:AFerreyrolles")));
-		     
-		  // Instantiating HTable class
-		      Table table = conn.getTable(TableName.valueOf("ece_2021_fall_app_2:AFerreyrolles"));
-		      
-		     // Instantiating Get class
-		      Get g = new Get(Bytes.toBytes("user"));
-		      
-		   // Reading the data
-		      Result result = table.get(g);
-		      
-		      byte[] value = result.getValue(Bytes.toBytes("username"), Bytes.toBytes(""));
-		      String valueStr = Bytes.toString(value);
-		      
-		      System.out.println("value : " + value);
+		try {
+			conn = HbaseConnector.getConnectionByFile("/home/a.ferreyrolles-ece/mykey.keytab",
+					"/etc/hadoop/conf/core-site.xml", "/etc/krb5.conf", "/etc/hbase/conf/hbase-site.xml",
+					"a.ferreyrolles-ece@AU.ADALTAS.CLOUD");
 
-		      System.out.println("valueStr : " + valueStr);
-		      return valueStr;	
-		 } catch (MasterNotRunningException e) {
-				e.printStackTrace();
-			} catch (ZooKeeperConnectionException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-				return "tamer";
+			Admin admin = conn.getAdmin();
+			
+			return "Connection success: " + admin.tableExists(TableName.valueOf("ece_2021_fall_app_2:AFerreyrolles"));
+		} catch (MasterNotRunningException e) {
+			e.printStackTrace();
+		} catch (ZooKeeperConnectionException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "Connection Failed";
+	}
 
-		
+	@GetMapping("/get_values")
+	public String getValues() {
+		// Instantiating HTable class
+		Table table;
+		try {
+			conn = HbaseConnector.getConnectionByFile("/home/a.ferreyrolles-ece/mykey.keytab",
+					"/etc/hadoop/conf/core-site.xml", "/etc/krb5.conf", "/etc/hbase/conf/hbase-site.xml",
+					"a.ferreyrolles-ece@AU.ADALTAS.CLOUD");
+
+			table = conn.getTable(TableName.valueOf("ece_2021_fall_app_2:AFerreyrolles"));
+			// Instantiating Get class
+			Get g = new Get(Bytes.toBytes("user"));
+
+			// Reading the data
+			Result result = table.get(g);
+
+			byte[] value = result.getValue(Bytes.toBytes("username"), Bytes.toBytes(""));
+			String valueStr = Bytes.toString(value);
+
+			System.out.println("value : " + value);
+
+			System.out.println("valueStr : " + valueStr);
+			return valueStr;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Couldn't get values";
 	}
 }
