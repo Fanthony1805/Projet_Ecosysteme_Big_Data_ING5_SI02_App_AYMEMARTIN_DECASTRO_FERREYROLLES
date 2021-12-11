b64c15ebfe28ef4ff9fef4e19c216706fe7aab4e
Anthony FERREYROLLES <anthony.ferreyrolles@edu.ece.fr>
2021-12-11 20:10:26 +0100

    Correction of the login class

--- big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/UserController.java
+++ big_data_project/src/main/java/com/ece_ing5app_2021/big_data_project/UserController.java
@@ -84,9 +84,9 @@ public class UserController {
 			String user_password = Bytes.toString(value);
 			
 			System.out.println("Pass 1 = " + user_password);
-			System.out.println("Pass 2 = " + user.get("username").toString());
+			System.out.println("Pass 2 = " + user.get("password").toString());
 			
-			return user_password.equals(user.get("username").toString());
+			return user_password.equals(user.get("password").toString());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
